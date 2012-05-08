715ef12fdf2fff081be195d91b18b331687b0b3e
kgokita <kgokita@kgokita-XPS.(none)>
2012-05-08 19:07:45 +0900

    定数の定義とか

--- redplanet.d
+++ redplanet.d
@@ -1,3 +1,11 @@
+/**
+ *  Authors:
+ *    @kazoo04
+ *
+ *  See_Also:
+ *    http://wllwmiilmmw.tumblr.com/post/21429448637/d-2
+ */
+
 import std.algorithm	: map, reduce;
 import std.base64	: Base64;
 import std.conv		: to;
@@ -7,9 +15,61 @@ import std.net.curl	: HTTP, post;
 import std.stdio	: write, writeln, readln;
 import std.string	: join, split, chomp, format, rightJustify;
 import std.uri		: encodeComponent;
+import std.regex;
+import std.stream;
 
 import deimos.openssl.ssl;
 
+enum Api : string
+{
+  host            = "http://api.twitter.com",
+  request_token   = host ~ "/oauth/request_token",
+  authorize       = host ~ "/oauth/authorize",
+  access_token    = host ~ "/oauth/access_token",
+  statuses_update = host ~ "/statuses/update.json",
+}
+
+enum Stream : string
+{
+  host = "https://userstream.twitter.com/2/",
+  user = host ~ "user.json",
+}
+
+string[char[]] loadKeys()
+{
+  Stream file = new BufferedFile("keys.conf");
+
+  string[char[]] keys;
+
+  foreach(char[] _line; file) {
+    string line = replace(cast(string)_line, regex(r"\s", "g"), "");
+    
+    if(line.length == 0) continue;
+    if(line[0] == '#') continue;
+
+    string key = split(line, "=")[0];
+    string val = split(line, "=")[1];
+
+    keys[key] = val;
+  }
+
+ 
+  return keys;
+}
+
+void main(string args[])
+{
+  auto join_query - (string[string] p) =>
+    p.keys.sort.map!(k => k ~
+  auto keys = loadKeys();
+
+  immutable consumer_key = keys["consumer_key"];
+  immutable consumer_secret = keys["consumer_secret"];
+
+
+}
+
+/++
 immutable consumer_key = ""
 
 void main(string args[])
@@ -38,3 +98,4 @@ void main(string args[])
     writeln(o.object["text"].str, " ", created_at(o.object["created_at"].str));
   }
 }
++/
