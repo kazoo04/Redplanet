834961e6fe3b8d638eb89bb75dff716492382fa0
kgokita <kgokita@kgokita-XPS.(none)>
2012-05-09 22:37:52 +0900

    UserStreamからTLを読み込むテスト

--- redplanet.d
+++ redplanet.d
@@ -15,10 +15,12 @@ import std.net.curl	: HTTP, post;
 import std.stdio	: write, writeln, readln;
 import std.string	: join, split, chomp, format, rightJustify;
 import std.uri		: encodeComponent;
-import std.regex;
-import std.stream;
 
 import deimos.openssl.ssl;
+
+
+immutable consumer_key = "gi9QcwbtYnsEKQf2HWBGg";
+immutable consumer_secret = "iztMAGvqAVKejVLRHwtxvQD251Z06C3MoWXjtDC6A";
 
 enum Api : string
 {
@@ -35,9 +37,10 @@ enum Stream : string
   user = host ~ "user.json",
 }
 
+/*
 string[char[]] loadKeys()
 {
-  Stream file = new BufferedFile("keys.conf");
+  auto file = new BufferedFile("keys.conf");
 
   string[char[]] keys;
 
@@ -52,21 +55,143 @@ string[char[]] loadKeys()
 
     keys[key] = val;
   }
-
  
   return keys;
 }
+*/
 
 void main(string args[])
 {
-  auto join_query - (string[string] p) =>
-    p.keys.sort.map!(k => k ~
-  auto keys = loadKeys();
+  //auto keys = loadKeys();
 
-  immutable consumer_key = keys["consumer_key"];
-  immutable consumer_secret = keys["consumer_secret"];
+  auto join_query = (string[string] p) =>
+    p.keys.sort.map!(k => k ~ "=" ~ p[k]).join("&");
+
+  auto split_query = (string query) {
+    string[string] p = null;
+    foreach(q; query.split("&")) {
+      auto s = q.split("=");
+      p[s[0]] = s[1];
+    }
+    return p;
+  };
+
+  auto hmac_sha1 = (string key, string data) {
+    auto result = new ubyte[SHA_DIGEST_LENGTH];
+    HMAC(EVP_sha1(), key.ptr, cast(int)key.length, cast(ubyte*)(data.ptr), 
+        data.length, result.ptr, null);
+    return result;
+  };
+
+  auto oauth_signature = (string method, string url, string query, string csec, string asec = null) {
+    auto base = [method, url, query].reduce!((xs, x) => xs ~ "&" ~ x.encodeComponent);
+    auto key = [csec, asec].reduce!((xs, x) => xs ~ "&" ~ x.encodeComponent);
+    return encodeComponent(cast(immutable)Base64.encode(hmac_sha1(key, base)));
+  };
+
+  auto oauth_header = (string[string] p) =>
+    "OAuth " ~ p.keys.sort.map!(k => k ~ "=\"" ~ p[k] ~ "\"").join(",");
+
+  auto oauth_post = (string uri, string[string] ps, string asec = null, string status = null) {
+    auto param = [
+      "oauth_consumer_key"      : consumer_key,
+      "oauth_nonce"             : Clock.currTime.toUnixTime.to!string,
+      "oauth_signature_method"  : "HMAC-SHA1",
+      "oauth_timestamp"         : Clock.currTime.toUnixTime.to!string,
+      "oauth_version"           : "1.0"];
+
+    foreach(k; ps.keys) param[k] = ps[k];
 
 
+    auto signature = oauth_signature("POST", uri, join_query(param), consumer_secret, asec);
+    param["oauth_signature"] = signature;
+
+    auto str = (status is null) ? "" : "status=" ~ status;
+    auto http = HTTP();
+    http.addRequestHeader("Authorization", oauth_header(param));
+
+    writeln("signature = ", signature);
+    writeln("param = ", param);
+    writeln("\turi = ", uri);
+    writeln("\tstr = ", str);
+    writeln("\thttp = ", http);
+
+    auto result = cast(immutable)post(uri, str, http);
+    /*
+    try {
+      auto result = cast(immutable)post(uri, str, http);
+    }
+    catch(CurlException) {
+      writeln("CurlException:");
+    }
+    */
+    writeln("\tresult = ", result);
+
+    return result;
+  };
+
+  // run from here
+  writeln("get request token");
+  auto request_token = (() =>
+      split_query(oauth_post(Api.request_token, null))) ();
+
+
+  writeln("oauth verify");
+  auto oauth_verifier = () {
+    writeln("open the following url and allow:");
+    writeln("\t" ~  Api.authorize ~ "?oauth_token=" ~ request_token["oauth_token"]);
+    writeln("input pin:\n\t");
+    return readln.chomp;
+  }();
+
+  writeln("get access token");
+  auto access_token = (() => split_query(
+        oauth_post(Api.access_token,
+          [ "oauth_verifier" : oauth_verifier, "oauth_token" : request_token["oauth_token"]],
+          request_token["oauth_token_secret"]))) ();
+
+
+  auto streaming = () {
+    auto param = [
+      "oauth_consumer_key"      : consumer_key,
+      "oauth_nonce"             : Clock.currTime.toUnixTime.to!string,
+      "oauth_signature_method"  : "HMAC-SHA1",
+      "oauth_timestamp"         : Clock.currTime.toUnixTime.to!string,
+      "oauth_version"           : "1.0",
+      "oauth_token"             : access_token["oauth_token"]];
+
+    auto signature = oauth_signature("GET", Stream.user,
+        join_query(param), consumer_secret, access_token["oauth_token_secret"]);
+    param["oauth_signature"] = signature;
+    param["replies"] = "all";
+
+    auto created_at = (JSONValue v) {
+      auto d = v.object["created_at"].str.split;
+      const t = SysTime(DateTime.fromSimpleString(
+        d[5] ~ "-" ~ d[1] ~ "-" ~ d[2] ~ " " ~ d[3])) + dur!"hours"(9);
+
+      return [t.hour, t.minute, t.second].map!(x => x.to!string.rightJustify(2, '0')).join(":");
+    };
+
+    auto screen_name = (JSONValue v) => v.object["user"].object["screen_name"].str;
+    auto text = (JSONValue v) => v.object["text"].str;
+
+    auto http = HTTP(Stream.user);
+    http.addRequestHeader("Authorization", oauth_header(param));
+    http.addRequestHeader("User-Agent", "the unspeakable one");
+    http.dataTimeout = dur!"seconds"(-1);
+    http.onReceive = (ubyte[] data) {
+      auto j = (cast(string)data == "\r\n") ? JSONValue() : (cast(string)data).parseJSON;
+      if("text" in j.object)
+        format("%-15s: %s at %s", screen_name(j), text(j), created_at(j)).writeln;
+      return data.length;
+    };
+
+    http.perform;
+
+    return true;
+
+  }();
 }
 
 /++
